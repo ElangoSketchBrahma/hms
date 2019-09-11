5f3decd680e69d6a95f2c833ad2437ce5221f778
Elango <46561931+Elangodevguy@users.noreply.github.com>
2019-09-11 19:52:52 +0530

    completed crud functionality for hotels in admin side

--- src/App.js
+++ src/App.js
@@ -1,67 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Header from './components/layout/header/header';
-import Hotels from './components/hotels/Hotels';
-import Rooms from './components/rooms/Rooms';
-import Users from './components/users/Users';
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
-  const [hotels, setHotels] = useState([]);
-
-  const hotelsList = [
-    {
-      id: 1,
-      name: 'Royal Park',
-      address: 'Banglore',
-      contact: '123456789'
-    },
-    {
-      id: 2,
-      name: 'Holiday In',
-      address: 'Banglore',
-      contact: '123456789'
-    },
-    {
-      id: 3,
-      name: 'Seven hills ',
-      address: 'Banglore',
-      contact: '123456789'
-    },
-    {
-      id: 4,
-      name: 'Manis Hotel',
-      address: 'Banglore',
-      contact: '123456789'
-    },
-    {
-      id: 5,
-      name: 'Le Meridian',
-      address: 'Banglore',
-      contact: '123456789'
-    }
-  ];
-  // setHotels(hotelsList);
-  // setHotels([1,2,3,4]);
-
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
-    <Router>
-      <div className='App'>
-        <Header />
-        <div className='container'>
-          <Switch>
-            <Route
-              exact
-              path='/admin/hotels'
-              render={props => <Hotels {...props} hotelsList={hotelsList} />}
-            />
-            <Route exact path='/admin/rooms' component={Rooms} />
-            <Route exact path='/admin/users' component={Users} />
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <div className='App'>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(10))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      {isLogged ? <h3>Valuable information I shouldn't see</h3> : ''}
+    </div>
   );
 }
 
